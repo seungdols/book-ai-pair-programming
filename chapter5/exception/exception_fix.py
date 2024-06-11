5fe7cf085c444254efc0a05abd3ef7438a7ae7c7
seungdols <seungdols0822@gmail.com>
2024-06-11 23:39:36 +0900

    Fix exception_fix.py

--- chapter5/exception/exception_fix.py
+++ chapter5/exception/exception_fix.py
@@ -1,20 +1,29 @@
 def main():
     try:
         divisor = 0
-        x = 1 / divisor
+        if divisor != 0:
+            x = 1 / divisor
+            print("Result : ", x)
+        else:
+            print("Cannot divide by zero")
     except Exception as e:
         print("Error", e)
 
     try:
         my_dict = {'name': 'Alice'}
-        age = my_dict.get('age')
-    except Exception as e:
-        print("Error", e)
+        name = my_dict.get('name')
+        if name is not None:
+            print("name: ", name)
+        else:
+            print("name not found")
+    except KeyError as e:
+        print("Error: key not found", e)
 
     try:
-        int('abc')  # ValueError
-    except Exception as e:
-        print("Error", e)
+        result = int('123')  # No ValueError
+        print("result: ", result)
+    except ValueError as e:
+        print("Error: Invalid value - ", e)
 
 if __name__ == "__main__":
     main()
